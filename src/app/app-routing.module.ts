66a7d44fa04784a1b48a517a8faf6e1643e4be9b
Ryan Brown <ryanbrown4610@gmail.com>
2022-12-02 11:07:35 -0500

    Course Report Modal Done Small issue with a white line on the right of modal header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,10 +14,9 @@ const routes: Routes = [
 		{ path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
 		{ path: 'manage-courses', loadChildren: () => import('./features/manage-courses/manage-courses.module').then(m => m.ManageCoursesModule), canActivate: [AuthGuard] },
 		{ path: 'degree-manager', loadChildren: () => import('./features/degree-manager/degree-manager.module').then(m => m.DegreeManagerModule) },
-		{ path: 'course-details', loadChildren: () => import('./features/course-details/course-details.module').then(m => m.CourseDetailsModule), canActivate: [AuthGuard], data: {navbar: "showExtra"}},	
+		{ path: 'course-details', loadChildren: () => import('./features/course-details/course-details.module').then(m => m.CourseDetailsModule), canActivate: [AuthGuard] },	
 	// ]},
 	{ path: '', component: NavbarComponent, outlet:'nav'},
-	{ path: 'main', component: NavbarComponent, outlet:'nav'},
 	{ path: 'course-details', component: NavbarCourseDetailsComponent, outlet:'nav'},
 	
 	// { path: 'sign-in', loadChildren: () => import('./features/sign-in/sign-in.module').then(m => m.SignInModule) },
